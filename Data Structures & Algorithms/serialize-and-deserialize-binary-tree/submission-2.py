dd073a2d364d0ab7d3cfbd831f8b712fd645e689
Gavin Wilson <65029789+opey9519@users.noreply.github.com>
2026-09-06 14:36:24 -0400

    Add: serialize-and-deserialize-binary-tree - submission-2

---
+++ Data Structures & Algorithms/serialize-and-deserialize-binary-tree/submission-2.py
@@ -0,0 +1,48 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Codec:
+    
+    # Encodes a tree to a single string.
+    def serialize(self, root: Optional[TreeNode]) -> str:
+        res = []
+
+        def dfs(root):
+            if not root:
+                res.append("N")
+                return
+            
+            res.append(str(root.val))
+            dfs(root.left)
+            dfs(root.right)
+        
+        dfs(root)
+        return ",".join(res)
+        
+    # Decodes your encoded data to tree.
+    def deserialize(self, data: str) -> Optional[TreeNode]:
+        # Split data into array & create global pointer
+        vals = data.split(",")
+        self.i = 0
+
+        def dfs():
+            # Base Case: There is no node
+            if vals[self.i] == "N":
+                self.i += 1
+                return None
+
+            # Create the node w/ val 
+            node = TreeNode(int(vals[self.i]))
+            self.i += 1
+            # Recursively create the children nodes & subtrees
+            node.left = dfs()
+            node.right = dfs()
+
+            return node
+
+        return dfs()
+

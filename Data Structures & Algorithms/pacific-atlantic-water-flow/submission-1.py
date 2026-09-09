7a314e09f77cdc7062ae5f3e2175b2b3aa271915
Gavin Wilson <65029789+opey9519@users.noreply.github.com>
2026-09-08 21:06:09 -0400

    Add: pacific-atlantic-water-flow - submission-1

---
+++ Data Structures & Algorithms/pacific-atlantic-water-flow/submission-1.py
@@ -0,0 +1,42 @@
+class Solution:
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        ROWS = len(heights)
+        COLS = len(heights[0])
+        pac = set()
+        atl = set()
+        
+        def dfs(r, c, visit, prevHeight):
+            # Base Case: out of bounds, already visited, cannot flow
+            if (r >= ROWS or c >= COLS or r < 0 or c < 0 or 
+                (r, c) in visit or prevHeight > heights[r][c]
+            ):
+                return 
+            
+            visit.add((r, c))
+
+            # Water flows
+            dfs(r + 1, c, visit, heights[r][c])
+            dfs(r - 1, c, visit, heights[r][c])
+            dfs(r, c + 1, visit, heights[r][c])
+            dfs(r, c - 1, visit, heights[r][c])
+
+        # Visit each position of Top and Bottom Row
+        for c in range(COLS):
+            dfs(0, c, pac, heights[0][c])
+            dfs(ROWS - 1, c, atl, heights[ROWS - 1][c])
+        
+        # Visit each position of Left and Right Columns
+        for r in range(ROWS):
+            dfs(r, 0, pac, heights[r][0])
+            dfs(r, COLS - 1, atl, heights[r][COLS - 1])
+        
+        res = []
+        # Append joint positions 
+        for r in range(ROWS):
+            for c in range(COLS):
+                if (r, c) in pac and (r, c) in atl:
+                    res.append([r, c])
+        
+        return res
+
+        
